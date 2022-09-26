68374706b570111e1b4a29f1efd784bd228cba2e
adut24 <dutertre.alexandre@laposte.net>
2022-09-26 15:48:13 +0200

    Adding new sprites

--- Assets/Scripts/RoomGenerator.cs
+++ Assets/Scripts/RoomGenerator.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class RoomGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
