af8b6e241dcf4d7f1710652b7404ea9d4c1c9959
Dureau <pierre.dureau53@laposte.net>
2022-10-07 13:20:29 +0200

    Basic player movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float timeOffset;
+    public Vector3 posOffset;
+
+    private Vector3 velocity;
+
+    void Start()
+    {
+        transform.position = player.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + posOffset, ref velocity, timeOffset);
+    }
+}
