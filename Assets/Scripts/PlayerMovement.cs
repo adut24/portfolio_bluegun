bc172ed4d2a49cc643b751a054d7aedb4cacd3ad
Dureau <dureaupierre53@gmail.com>
2022-10-07 15:10:18 +0200

    Add Player Dash

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb;
-
     private Vector2 moveDirection;
+
+    private bool canDash = true;
+    private bool isDashing = false;
+    private float dashingPower = 24f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 2f;
+    public TrailRenderer tr;
 
     void Update() //Player Inputs
     {
+        if (isDashing)
+        {
+            return;
+        }
+
         ProcessInputs();
+
+        if (Input.GetKeyDown(KeyCode.Space) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
     }
 
     void FixedUpdate() //Physics Calculation
     {
+        if (isDashing)
+        {
+            return;
+        }
+
         Move();
     }
 
@@ -28,5 +51,18 @@ public class PlayerMovement : MonoBehaviour
     void Move()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        rb.velocity = new Vector2(moveDirection.x * dashingPower, moveDirection.y * dashingPower);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
