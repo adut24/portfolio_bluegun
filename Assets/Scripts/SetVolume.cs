695c2460d91e048ea4bc08d0b0c29fb9310160ed
Nicolas Boute <4249@holbertonschool.com>
2022-10-12 15:07:28 +0200

    merged script

---
+++ Assets/Scripts/SetVolume.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public TextMeshProUGUI volumeText;
+
+    void   Start()
+    {
+        volumeText = GameObject.Find("VolumeValue").GetComponent<TextMeshProUGUI>();
+        int volValue = (int) MathF.Floor(this.GetComponent<Slider>().value * 100);
+        volumeText.text = volValue.ToString();
+    }
+    public void SetLevel (float sliderValue)
+    {
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+        volumeText.text = (MathF.Floor(sliderValue * 100)).ToString();
+    }
+}
