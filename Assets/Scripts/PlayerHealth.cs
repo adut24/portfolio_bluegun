274e2f61f2be0c97cc1803542bccdea6e3a0dcea
Dureau <dureaupierre53@gmail.com>
2022-10-14 11:14:00 +0200

    Add start of the inventory + invincibility

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 100;
     [SerializeField] private int currenthealth;
     [SerializeField] private HealthBar hb;
+    [SerializeField] private bool isInvicible = false;
+    [SerializeField] private SpriteRenderer graphics;
 
     private void Start()
     {
@@ -14,7 +17,30 @@ public class PlayerHealth : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
-        currenthealth -= damage;
-        hb.SetHealth(currenthealth);
+        if (!isInvicible)
+        {
+            currenthealth -= damage;
+            hb.SetHealth(currenthealth);
+            isInvicible = true;
+            StartCoroutine(InvincibilityFlash());
+            StartCoroutine(InvincibilityDelay());
+        }    
+    }
+
+    public IEnumerator InvincibilityFlash()
+    {
+        while (isInvicible)
+        {
+            graphics.color = new Color(1f, 1f, 1f, 0f);
+            yield return new WaitForSeconds(0.15f);
+            graphics.color = new Color(1f, 1f, 1f, 1f); 
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+    public IEnumerator InvincibilityDelay()
+    {
+        yield return new WaitForSeconds(1f);
+        isInvicible = false;
     }
 }
