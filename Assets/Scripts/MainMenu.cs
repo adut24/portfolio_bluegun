b8f29bd887c3e9e869d1437450c599b886554489
Nicolas Boute <4249@holbertonschool.com>
2022-10-14 16:17:54 +0200

    fixed game objects not being destroyed on game quit

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,13 +8,17 @@ public class MainMenu : MonoBehaviour
     public KeepOnLoad script = null;
     public void ChangeScene (string sceneName)
     {
+    	UnityEngine.Debug.Log("before");
         if (script)
         {
+            UnityEngine.Debug.Log("this should print");
             foreach (GameObject element in script.objects)
             {
                 Destroy(element);
             }
         }
+        UnityEngine.Debug.Log("after");
+        
         SceneManager.LoadScene(sceneName);
     }
     
