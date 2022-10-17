2ee4002f16d8567188f2df57a49f26fbbe700864
niboute <noucoulous53@gmail.com>
2022-10-17 23:33:27 +0200

    Added projectile settings and textures

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float    speed = 4.5f;
+    public Vector2  direction;
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
